a9e3faa9d9700127739a4325c5951ea3ec77785e
TrungPhuNA <phupt.humg.94@gmail.com>
2017-08-09 17:29:27 +0700

    8092017

---
+++ index.php
@@ -0,0 +1,297 @@
+
+<!DOCTYPE html>
+<html lang="">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title> TRAINING  HTML CSS JS PHP </title>
+		<link rel="stylesheet" href="css/bootstrap.min.css">
+		<link rel="stylesheet" href="scss/style.css?v=<?php echo rand(1,1000) ?>">
+		<style type="text/css">
+			
+		</style>
+	</head>
+	<body>
+		<div class="container" id="main">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">TỔNG HỢP KIẾN THỨC HTML</div>
+                        <div class="panel-body">
+                            <div class="panel-group" id="html">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#html" href="#html1"><span class="text-black-bold">Bài 01</span> : Khái niệm HTML. Bố cục một website</a>
+                                        </h4>
+                                    </div>
+                                    <div id="html1" class="panel-collapse collapse in">
+                                        <div class="panel-body">
+                                            <ul>
+                                                <li>HTML là chữ viết tắt của <span class="text-red-bold">HyperText Markup Language</span>, có nghĩa là ngôn ngữ đánh dấu siêu văn bản.</li>
+                                                <li>
+                                                    Bố cục
+                                                    <img src="images/1.jpg" class="img img-responsive hover-zoom" title=" Bố cục một website ">
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#html" href="#html2"><span class="text-black-bold">Bài 02</span> : Elements ( thẻ HTML ) và <span>Attributes</span> ( thuộc tính ) </a>
+                                        </h4>
+                                    </div>
+                                    <div id="html2" class="panel-collapse collapse">
+                                        <div class="panel-body">
+                                            <ul>
+                                                <li> <span class="text-red-bold">Elements</span>: Có rất nhiều thẻ </li>
+                                                <ol>
+                                                    <li> h1,h2...h6,div,a,p,</li>
+                                                    <li> input, link, meta, </li>
+                                                    <li> Và nhiều thẻ khác nữa </li>
+                                                    <li> Ví dụ <img src="images/3.jpg" class="img img-responsive hover-zoom" title=" Ví dụ về thẻ HTML "></li>
+                                                </ol>
+                                            </ul>
+                                            <ul>
+                                                <li><span class="text-red-bold">Attributes</span>: Thuộc tính </li>
+                                                <ol>
+                                                    <li> Ví dụ một số thuộc tính của các thẻ như <img src="images/2.jpg" class="img img-responsive hover-zoom" title=" Ví dụ về thuộc tính"></li>
+                                                </ol>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#html" href="#html3"><span class="text-black-bold">Bài 03</span> : Các thẻ HTML định dạng văn bản  </a>
+                                        </h4>
+                                    </div>
+                                    <div id="html3" class="panel-collapse collapse">
+                                        <div class="panel-body">
+                                            <ol>
+                                                <li> Ví dụ như <img src="images/4.jpg" class="img img-responsive hover-zoom" title="Thẻ định dạng HMTL "></li>
+                                            </ol>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#html" href="#html4"><span class="text-black-bold">Bài 04</span> : Các thẻ định dạng Heading và List  </a>
+                                        </h4>
+                                    </div>
+                                    <div id="html4" class="panel-collapse collapse">
+                                        <div class="panel-body">
+                                            <ol>
+                                                <li> Thẻ <span class="text-red-bold">Heading</span> <img src="images/5.jpg" class="img img-responsive hover-zoom" title="Thẻ định dạng Heading "></li>
+                                                <li> Thẻ <span class="text-red-bold">List</span> <img src="images/6.jpg" class="img img-responsive hover-zoom" title="Thẻ định dạng List "></li>
+                                            </ol>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#html" href="#html5"><span class="text-black-bold">Bài 05</span> : Các thẻ định dạng Table  </a>
+                                        </h4>
+                                    </div>
+                                    <div id="html5" class="panel-collapse collapse">
+                                        <div class="panel-body">
+                                            <ol>
+                                                <li> Thẻ <span class="text-red-bold">Table </span> <img src="images/7.jpg" class="img img-responsive hover-zoom" title="Thẻ định dạng Table "></li>
+                                                
+                                            </ol>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#html" href="#html6"><span class="text-black-bold">Bài 06</span> : Các thẻ tạo Link , Ảnh   </a>
+                                        </h4>
+                                    </div>
+                                    <div id="html6" class="panel-collapse collapse">
+                                        <div class="panel-body">
+                                            <ol>
+                                                <li> Thẻ <span class="text-red-bold">Link </span> <img src="images/8.jpg" class="img img-responsive hover-zoom" title="Thẻ định dạng Link  "></li>
+                                                <li> Thẻ <span class="text-red-bold">Tạo ảnh (img) </span> <img src="images/9.jpg" class="img img-responsive hover-zoom" title="Thẻ định dạng Ảnh  "></li>
+                                                
+                                            </ol>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">TỔNG HỢP KIẾN THỨC CSS</div>
+                        <div class="panel-body">
+                            <ol>
+                                <li></li>
+                            </ol>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">TỔNG HỢP KIẾN THỨC JQUERY</div>
+                        <div class="panel-body">
+                            <div class="panel-group" id="jquery">
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#jquery" href="#jquery1"><span class="text-black-bold">Bài 01</span> : Cú pháp</a>
+                                        </h4>
+                                    </div>
+                                    <div id="jquery1" class="panel-collapse collapse">
+                                        <div class="panel-body">
+                                            <ol>
+                                                <li>
+                                                    <div class="alert alert-success">
+                                                        $(this).hide()
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="alert alert-success">
+                                                        $("p").hide()
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="alert alert-success">
+                                                        $(".test").hide()
+                                                    </div>
+                                                </li>
+                                                <li>
+                                                    <div class="alert alert-success">
+                                                        $("#test").hide()
+                                                    </div>
+                                                </li>
+                                            </ol>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#jquery" href="#jquery2"><span class="text-black-bold">Bài 02</span> : Bộ chọn Selector</a>
+                                        </h4>
+                                    </div>
+                                    <div id="jquery2" class="panel-collapse collapse">
+                                        <div class="panel-body">
+                                            <ol>
+                                               
+                                            </ol>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#jquery" href="#jquery3"><span class="text-black-bold">Bài 03</span> : Event </a>
+                                        </h4>
+                                    </div>
+                                    <div id="jquery3" class="panel-collapse collapse">
+                                        <div class="panel-body">
+                                            <ol>
+                                                <li>Mouse
+                                                    <ul>
+                                                        <li>Click</li>
+                                                        <li>dblclick</li>
+                                                        <li>mouseenter ( Chuột nhấp vào phần tử)</li>
+                                                        <li>mouseleave ( Ngược lại ) </li>
+                                                    </ul>
+                                                </li>
+
+                                                <li>Keyboard ( bàn phím ) 
+                                                    <ul>
+                                                        <li>keypress</li>
+                                                        <li>keydown</li>
+                                                        <li>keyup</li>
+                                                    </ul>
+                                                </li>
+
+                                                <li> Form 
+                                                    <ul>
+                                                        <li>submit</li>
+                                                        <li>Change</li>
+                                                        <li>focus</li>
+                                                        <li>blur</li>
+                                                    </ul>
+                                                </li>
+
+                                                <li>Document/Window Events
+                                                    <ul>
+                                                        <li>Load</li>
+                                                        <li>Resize</li>
+                                                        <li>Scroll</li>
+                                                        <li>unload</li>
+                                                    </ul>
+                                                </li>
+                                                <li> On ( Gắn nhiều sự kiện vào 1 phần tử)</li>
+                                                
+                                            </ol>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                 <div class="panel panel-default">
+                                    <div class="panel-heading">
+                                        <h4 class="panel-title">
+                                            <a data-toggle="collapse" data-parent="#jquery" href="#jquery4"><span class="text-black-bold">Bài 04</span> : Hiệu ứng Animate </a>
+                                        </h4>
+                                    </div>
+                                    <div id="jquery4" class="panel-collapse collapse">
+                                        <div class="panel-body">
+                                            <ol>
+                                               <li>Hide/Show</li>
+                                               <li>Fade</li>
+                                               <li>Slide</li>
+                                               <li>Animate</li>
+                                               <li>Stop()</li>
+                                               <li>Callback</li>
+                                               <li>Chaining</li>
+                                            </ol>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>  
+		</div>	
+		<!-- jQuery -->
+		<script src="js/jquery.min.js"></script>
+		<!-- Bootstrap JavaScript -->
+		<script src="js/bootstrap.min.js"></script>
+		<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+ 	
+	</body>
+</html>
+<script type="text/javascript">
+    $(function(){
+        let $hover_zoom = $(".hover-zoom");
+        $hover_zoom.on({
+            click:function()
+            {
+                $(this).addClass('hover-zoom-click');
+            },
+            dblclick:function()
+            {
+                $(this).removeClass('hover-zoom-click');
+            }
+        });
+    })
+</script>
